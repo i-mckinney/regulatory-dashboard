a17b83b3d5f8af7d5d24ae0982f9bad65ba71c9a
Eric <ejho13@berkeley.edu>
2020-08-20 11:42:56 -0700

    Admin Navbar Material UI first draft

--- Dashboard/src/App.js
+++ Dashboard/src/App.js
@@ -2,14 +2,10 @@ import React from "react"
 import { BrowserRouter } from "react-router-dom"
 import Navigation from "./routes-nav/Navigation"
 import Routes from "./routes-nav/Routes"
-import helixLogo from "./helixLogo.jpg"
 
 function App(history) {
   return (
     <BrowserRouter>
-      <div>
-        <img src={helixLogo} className="mt-2 mb-2" alt="helixLogo" />
-      </div>
       <div className="App">
         <Navigation />
         <Routes />
--- Dashboard/src/routes-nav/Navigation.js
+++ Dashboard/src/routes-nav/Navigation.js
@@ -1,51 +1,94 @@
-import React, { useState } from "react"
+import React from "react"
+import { useHistory } from "react-router-dom"
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from "reactstrap"
+  BottomNavigation,
+  BottomNavigationAction,
+  makeStyles,
+} from "@material-ui/core"
+import Computer from "@material-ui/icons/Computer"
+import FeaturedPlayListIcon from "@material-ui/icons/FeaturedPlayList"
+import AccountCircle from "@material-ui/icons/AccountCircle"
+import CreditCard from "@material-ui/icons/CreditCard"
+import AccountBalance from "@material-ui/icons/AccountBalance"
+import helixLogo from "../helixLogo.jpg"
+
+const useStyles = makeStyles({
+  root: {
+    display: "grid",
+  },
+
+  homeButton: {
+    backgroundColor: "white",
+    border: "none",
+    width: "300px",
+    marginTop: "15px",
+    gridArea: "1 / 1 / row1-end / 3",
+  },
+
+  hello: {
+    marginTop: "15px",
+    marginRight: "15px",
+    gridArea: "1 / 4 / row1-end / 6",
+  },
+})
 
 /** @returns {jsx} Handles Navigation for Helix Regulatory DashBoard. */
 const Navigation = () => {
-  // When the browser window gets to small, nav bar collapses and toggle is used to show and hide the navbar.
-  const [isOpen, setIsOpen] = useState(false)
-  const toggle = () => setIsOpen(!isOpen)
+  const history = useHistory()
+  const classes = useStyles()
+  const [value, setValue] = React.useState(0)
+
+  const handleHome = () => {
+    setValue(0)
+    history.push(`/`)
+  }
 
   return (
     <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">
-          <b>Home</b>
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/dashboard">Dashboard</NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink href="/entity">Entity</NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink href="/loan">Loan</NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink href="/regulatory">Regulatory</NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink href="/myrequest">My Request</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+      <div className={classes.root}>
+        <button
+          onClick={handleHome}
+          type="button"
+          className={classes.homeButton}
+        >
+          <img src={helixLogo} alt="helixLogo" />
+        </button>
+        <BottomNavigation
+          value={value}
+          className={classes.hello}
+          onChange={(event, newValue) => {
+            setValue(newValue)
+            history.push(`/${newValue}`)
+          }}
+          showLabels
+        >
+          <BottomNavigationAction
+            value="Dashboard"
+            label="Dashboard"
+            icon={<Computer />}
+          />
+          <BottomNavigationAction
+            value="Entity"
+            label="Entity"
+            icon={<AccountBalance />}
+          />
+          <BottomNavigationAction
+            value="Loan"
+            label="Loan"
+            icon={<CreditCard />}
+          />
+          <BottomNavigationAction
+            label="Regulatory"
+            value="Regulatory"
+            icon={<FeaturedPlayListIcon />}
+          />
+          <BottomNavigationAction
+            value="myrequest"
+            label="My Request"
+            icon={<AccountCircle />}
+          />
+        </BottomNavigation>
+      </div>
     </div>
   )
 }
