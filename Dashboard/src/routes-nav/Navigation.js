80fb0c8c123f7ba05e28959db444392ae73d5a33
Eric <ejho13@berkeley.edu>
2020-08-28 11:29:29 -0700

    Added Nav in cofirmation page react strap to allow working in local branch

--- Dashboard/src/routes-nav/Navigation.js
+++ Dashboard/src/routes-nav/Navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import {
@@ -6,15 +6,46 @@ import {
   Toolbar,
   Typography,
   Link,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@material-ui/core";
 import Computer from "@material-ui/icons/Computer";
 import FeaturedPlayListIcon from "@material-ui/icons/FeaturedPlayList";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import CreditCard from "@material-ui/icons/CreditCard";
 import AccountBalance from "@material-ui/icons/AccountBalance";
+import helixLogo from "../helixLogo.jpg";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  companySelectContainer: {
+    marginBottom: "20px",
+  },
+  helixLogoImg: {
+    backgroundColor: "white",
+    border: "none",
+    "&:hover": {
+      cursor: "pointer",
+    },
+    "&:active": {
+      marginTop: "15px",
+      marginBottom: 0,
+      marginRight: 5,
+    },
+    marginTop: "15px",
+    marginBottom: 0,
+    marginRight: 5,
+  },
+
+  companySelectField: {
+    minWidth: "300px",
+  },
+  companyFormControl: {
+    marginTop: "15px",
+    minWidth: 120,
+  },
+  navBarContainer: {
     flexGrow: 1,
   },
   navBar: {
@@ -24,7 +55,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 2,
     "&:hover": {
       cursor: "pointer",
-      color: "#0d47a1"
+      color: "#0d47a1",
     },
   },
   link: {
@@ -37,61 +68,99 @@ const useStyles = makeStyles((theme) => ({
   },
   icon: {
     marginRight: theme.spacing(0.5),
-    marginTop: theme.spacing(0.3),
+    marginTop: ".8px",
     width: 20,
     height: 20,
   },
 }));
 
-/** @returns {jsx} Handles Navigation for Helix Regulatory DashBoard. */
-const Navigation = () => {
+/**@return {jsx} returns Navigation component that is shared across micro services
+ * Contains company selection that will set company view
+ * Contains navbar to handle navigation in dashboard component
+ */
+function Navigation() {
+  //History is used to handle page routes
   const history = useHistory();
+  //useStyles define styling for material ui. Any className that matches key of useStyles object above,
+  // corresponding styles will be applied
   const classes = useStyles();
 
+  //when home button is clicked (helix logo) redirects to home page
   const handleHome = () => {
-    history.push(`/`);
+    history.push(`/home`);
+    window.location.reload(false);
   };
 
+  //This will keep track of what the user selects in company view select field
+  const [companyView, setCompanyView] = useState("");
+  //will be used in the future to send display different nav pages
+  const handleCompanyViewChange = (event) => {
+    setCompanyView(event.target.value);
+  };
   return (
-    <div className={classes.root}>
-      <AppBar position="static" className={classes.navBar}>
-        <Toolbar>
-          <Typography
-            variant="h6"
-            onClick={handleHome}
-            className={classes.homeButton}
+    <header>
+      <div className={classes.companySelectContainer}>
+        <img
+          className={classes.helixLogoImg}
+          src={helixLogo}
+          onClick={handleHome}
+          alt="helixLogo"
+        />
+        <FormControl className={classes.companyFormControl}>
+          <InputLabel id="company-select-label">Company View</InputLabel>
+          <Select
+            labelId="company-select"
+            id="company-select"
+            className={classes.companySelectField}
+            value={companyView}
+            onChange={handleCompanyViewChange}
           >
-            Home
-          </Typography>
+            <MenuItem value={""}></MenuItem>
+            <MenuItem value={"Microsoft"}>Microsoft</MenuItem>
+            <MenuItem value={"SalesForce"}>Sales Force</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className={classes.navBarContainer}>
+        <AppBar position="static" className={classes.navBar}>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              onClick={handleHome}
+              className={classes.homeButton}
+            >
+              Home
+            </Typography>
 
-          <Link color="inherit" href="/dashboard" className={classes.link}>
-            <Computer className={classes.icon} />
-            Dashboard
-          </Link>
+            <Link color="inherit" href="/dashboard" className={classes.link}>
+              <Computer className={classes.icon} />
+              Dashboard
+            </Link>
 
-          <Link color="inherit" href="/entity" className={classes.link}>
-            <AccountBalance className={classes.icon} />
-            Entity
-          </Link>
+            <Link color="inherit" href="/entity" className={classes.link}>
+              <AccountBalance className={classes.icon} />
+              Entity
+            </Link>
 
-          <Link color="inherit" href="/loan" className={classes.link}>
-            <CreditCard className={classes.icon} />
-            Loan
-          </Link>
+            <Link color="inherit" href="/loan" className={classes.link}>
+              <CreditCard className={classes.icon} />
+              Loan
+            </Link>
 
-          <Link color="inherit" href="/regulatory" className={classes.link}>
-            <FeaturedPlayListIcon className={classes.icon} />
-            Regulatory
-          </Link>
+            <Link color="inherit" href="/regulatory" className={classes.link}>
+              <FeaturedPlayListIcon className={classes.icon} />
+              Regulatory
+            </Link>
 
-          <Link color="inherit" href="/myrequest" className={classes.link}>
-            <AccountCircle className={classes.icon} />
-            My Request
-          </Link>
-        </Toolbar>
-      </AppBar>
-    </div>
+            <Link color="inherit" href="/myrequest" className={classes.link}>
+              <AccountCircle className={classes.icon} />
+              My Request
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </header>
   );
-};
+}
 
 export default Navigation;
