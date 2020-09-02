523d3c862d9c746c6793deba514417555d3c8bd3
Raymond Huang <raymond.huang@helixcp.com>
2020-09-01 22:08:04 -0700

    Add UserCreate to create user

--- userapp/src/components/user/UserCreate.js
+++ userapp/src/components/user/UserCreate.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+import UserForm from './UserForm'
 
+const initialUser = {
+    FirstName: "",
+    LastName: "",
+    DateOfBirth: "",
+    Phone: "",
+  }
 
-const UserCreate = () => {
-    return (<div>UserCreate</div>)
+  /**
+ * @return {JSX} UserCreate site
+ * routed at /user/new
+ */
+const UserCreate = ({ history }) => {
+
+    const onSubmit = (userValues) => {
+        createUser(userValues)
+    }
+
+    const createUser = (userValues) => {
+        console.log(userValues)
+    }
+    return (
+    <div>
+        <UserForm initialUser={initialUser} onSubmit={onSubmit} />
+    </div>
+    )
 }
 
-export default UserCreate
+export default withRouter(UserCreate)
