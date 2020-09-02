1d96dc1f087d4c85c71d357000602a5952ae74af
Raymond Huang <raymond.huang@helixcp.com>
2020-09-02 12:51:26 -0700

    Added HelixTextField and Add missing documentation

--- userapp/src/components/user/UserForm.js
+++ userapp/src/components/user/UserForm.js
@@ -1,53 +1,61 @@
 import React, { useState } from 'react'
-import { makeStyles,
-    TextField,
-    Grid,
-}  from '@material-ui/core';
+import { makeStyles, Grid }  from '@material-ui/core';
+import HelixTextField from '../controls/HelixTextField'
 
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      '& .MuiFormControl-root': {
-        width: '80%',
-        margin: theme.spacing(1),
-      },
-      '& > *': {
-        margin: theme.spacing(1),
-      },
+// Styling used for MaterialUi
+const userFormStyles = makeStyles((theme) => ({
+root: {
+    '& .MuiFormControl-root': {
+    width: '80%',
+    margin: theme.spacing(1),
     },
-  }));
+    '& > *': {
+    margin: theme.spacing(1),
+    },
+},
+}));
 
 /**
+ * @param {object} initialUser object with preset empty data
  * @return {JSX} UserCreate site
  * routed at /user/new
  */
 const UserForm = ({ initialUser }) => {
+    // Set user with preset empty data for user creation e.g. { FirstName: "", LastName: "", ...}
     const [user, setUser] = useState(initialUser);
-    const classes = useStyles();
-    
 
-    const handleInputChange = ({ target: { name, value } }) => {
+    // Creates an object for styling. Any className that matches key in the userFormStyles object will have a corresponding styling
+    const userFormClasses = userFormStyles();
+    
+    /**
+     * @param {Object} event the event object
+     * name: the name property on the target text field element
+     * value: the value property on the target text field element
+     */
+    const handleInputChange = (event) => {
+        const { name, value } = event.target
         setUser({ ...user, [name]: value })
     }
 
     return (
     <div>
-        <form className={classes.root} autoComplete="off" >
+        <form className={userFormClasses.root} autoComplete="off" >
          {/* onSubmit={handleSubmit}> */}
             <Grid container>
                 <Grid item xs={3}>
-                    <TextField
+                    <HelixTextField
                         name='FirstName'
                         label='First Name'
                         value={user.FirstName}
                         onChange={handleInputChange}
                     />
-                    <TextField
+                    <HelixTextField
                         name='LastName'
                         label='Last Name'
                         value={user.LastName}
                         onChange={handleInputChange}
                     />
-                    <TextField
+                    <HelixTextField
                         name='DateOfBirth'
                         label='Date Of Birth'
                         value={user.DateOfBirth}
@@ -55,7 +63,6 @@ const UserForm = ({ initialUser }) => {
                     />
                 </Grid>
                 <Grid item xs={6}>
-                    Hello
                     {/* <div style={buttonStyle}>
                         <Controls.Button type='submit' size='large' text='Save' />
                         <Controls.Button
