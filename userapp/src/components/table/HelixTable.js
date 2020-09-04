98a4a89026c8abd25ecd3e1f3f671661029f5658
Raymond Huang <raymond.huang@helixcp.com>
2020-09-04 14:30:20 -0700

    Merge branch user-microser into feature/microservice-usertable and Add TableSortLabel for TableSorting

--- userapp/src/components/table/HelixTable.js
+++ userapp/src/components/table/HelixTable.js
@@ -19,7 +19,11 @@ const HelixTable = ({
   customCellRender,
   customHeadRowProps,
   customBodyRowProps,
-}) => {
+  visuallyHidden,
+  order,
+  orderBy,
+  onRequestSort,
+  }) => {
   // Page is needed for pagination to determine the process of what page it is at
   const [page, setPage] = useState(0)
 
@@ -43,12 +47,38 @@ const HelixTable = ({
     setPage(0)
   }
 
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1
+    }
+    return 0;
+  }
+  
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy)
+  }
+  
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index])
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    })
+    return stabilizedThis.map((el) => el[0])
+  }
+
   return (
     <div>
       <TableContainer component={Paper}>
         <Table aria-label="table">
-          <HelixTableHead columns={columns} customHeadRowProps={customHeadRowProps}/>
-          <HelixTableBody columns={columns} rows={rows} rowsPerPage={rowsPerPage} page={page} customCellRender={customCellRender} customBodyRowProps={customBodyRowProps}/>
+          <HelixTableHead visuallyHidden={visuallyHidden} order={order} orderBy={orderBy} onRequestSort={onRequestSort} columns={columns} customHeadRowProps={customHeadRowProps}/>
+          <HelixTableBody descendingComparator={descendingComparator} getComparator={getComparator} stableSort={stableSort} columns={columns} rows={rows} rowsPerPage={rowsPerPage} page={page} customCellRender={customCellRender} customBodyRowProps={customBodyRowProps}/>
           <HelixTableFooter rows={rows} colSpan={columns.length} rowsPerPage={rowsPerPage} page={page} handleChangePage={handleChangePage} handleChangeRowsPerPage={handleChangeRowsPerPage} />
         </Table>
       </TableContainer>
--- userapp/src/components/table/HelixTableHead.js
+++ userapp/src/components/table/HelixTableHead.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { TableHead, TableRow, TableCell } from "@material-ui/core"
+import { TableHead, TableRow, TableCell, TableSortLabel } from "@material-ui/core"
 import PropTypes from "prop-types"
 
 /**
@@ -7,13 +7,26 @@ import PropTypes from "prop-types"
  * @param {func} customHeadRowProps func represents custom function that return key props for table row in table head (required)
  * @returns {JSX} renders a custom table head for table
  */
-const HelixTableHead = ({ columns, customHeadRowProps }) => {
+const HelixTableHead = ({ columns, customHeadRowProps, visuallyHidden, order, orderBy, onRequestSort }) => {
   return (
     <TableHead>
       <TableRow>
         {columns.map((column) => (
-            <TableCell key={customHeadRowProps(column)}>
-              {column.Label}
+            <TableCell 
+            key={customHeadRowProps(column)}
+            sortDirection={orderBy === customHeadRowProps(column) ? order : false} >
+              <TableSortLabel
+              active={orderBy === customHeadRowProps(column)}
+              direction={orderBy === customHeadRowProps(column) ? order : 'asc'}
+              onClick={onRequestSort(customHeadRowProps(column))}
+              >
+                {column.Label}
+                {orderBy === customHeadRowProps(column) ? (
+                <span className={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+              </TableSortLabel>
             </TableCell>
           ))}
       </TableRow>
@@ -24,6 +37,10 @@ const HelixTableHead = ({ columns, customHeadRowProps }) => {
 HelixTableHead.propTypes = {
   columns: PropTypes.instanceOf(Array).isRequired,
   customHeadRowProps: PropTypes.func.isRequired,
+  visuallyHidden: PropTypes.object.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
 }
 
 export default HelixTableHead
