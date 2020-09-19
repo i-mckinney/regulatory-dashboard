3b52ab68aa010b19caf0cd5143ddf837cffa3912
Taharka Taylor <taharka.taylor@helixcp.com>
2020-09-18 18:34:02 -0700

    created microservice for login page

--- packages/AppContainer/src/App.js
+++ packages/AppContainer/src/App.js
@@ -1,62 +1,68 @@
-import React from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-import PropTypes from "prop-types"
-import Header from "./Components/Header/Header"
-import MicroserviceLoader from "./MicroserviceLoader"
-import SpogPage from "./Components/Spog/SpogPage"
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Header from './Components/Header/Header';
+import MicroserviceLoader from './MicroserviceLoader';
+import SpogPage from './Components/Spog/SpogPage';
 
 const {
   REACT_APP_DASHBOARD_HOST: dashboardHost,
   REACT_APP_COMPANY_VIEW_HOST: companyViewHost,
   REACT_APP_USER_HOST: userHost,
-} = process.env
+  REACT_APP_LOGIN_HOST: loginViewHost,
+} = process.env;
 
 const Dashboard = ({ history }) => (
-  <MicroserviceLoader history={history} host={dashboardHost} name="Dashboard" />
-)
+  <MicroserviceLoader history={history} host={dashboardHost} name='Dashboard' />
+);
 
 const CompanyView = ({ history }) => (
   <MicroserviceLoader
     history={history}
     host={companyViewHost}
-    name="CompanyView"
+    name='CompanyView'
   />
-)
+);
+
+const LoginView = ({ history }) => (
+  <MicroserviceLoader history={history} host={loginViewHost} name='LoginView' />
+);
 
 const User = ({ history }) => (
-  <MicroserviceLoader history={history} host={userHost} name="User" />
-)
+  <MicroserviceLoader history={history} host={userHost} name='User' />
+);
 
 const App = () => (
   <BrowserRouter>
     <>
       <Header />
       <Switch>
-        <Route exact path="/" component={SpogPage} />
-        <Route exact path="/home" component={Dashboard} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/company/:id" component={CompanyView} />
-        <Route exact path="/entity" component={Dashboard} />
-        <Route exact path="/loan" component={Dashboard} />
-        <Route exact path="/regulatory" component={Dashboard} />
-        <Route exact path="/myrequest" component={Dashboard} />
-        <Route exact path="/users" component={User} />
-        <Route exact path="/users/new" component={User} />
-        <Route exact path="/users/edit/:id" component={User} />
-        <Route exact path="/users/delete/:id" component={User} />
+        <Route exact path='/' component={LoginView} />
+        <Route exact path='/spogpage' component={SpogPage} />
+        <Route exact path='/home' component={Dashboard} />
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/company/:id' component={CompanyView} />
+        <Route exact path='/entity' component={Dashboard} />
+        <Route exact path='/loan' component={Dashboard} />
+        <Route exact path='/regulatory' component={Dashboard} />
+        <Route exact path='/myrequest' component={Dashboard} />
+        <Route exact path='/user' component={User} />
+        <Route exact path='/user/new' component={User} />
+        <Route exact path='/user/edit/:id' component={User} />
+        <Route exact path='/user/delete/:id' component={User} />
       </Switch>
     </>
   </BrowserRouter>
-)
+);
 
 Dashboard.propTypes = {
   history: PropTypes.oneOfType([PropTypes.object]).isRequired,
-}
+};
 CompanyView.propTypes = {
   history: PropTypes.oneOfType([PropTypes.object]).isRequired,
-}
+};
 User.propTypes = {
   history: PropTypes.oneOfType([PropTypes.object]).isRequired,
-}
+};
 
-export default App
+export default App;
