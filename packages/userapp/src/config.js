3f9d46bf19b48d30f15bcdbf6aecfb5de9522c7b
Chia Huang <chialh@uci.edu>
2020-09-18 23:58:18 -0700

    Add configs for options for sortable and display column

---
+++ packages/userapp/src/config.js
@@ -0,0 +1,12 @@
+export const optionSortableExcludes = ["_id", "createdAt", "updatedAt", "MiddleName", "PhoneType", "Actions", "Address1", "Address2", "City", "State", "Country", "Zip"]
+export const columnExcludes = []
+/**
+"Username": "",
+"Firstname": "",
+"Lastname": "",
+"Phone": "",
+"Email": "",
+"JoinDate": "",
+"Role": ""
+* 
+*/
