90daa787854e498aa94ac8ec940add854159bb9c
i-mckinney <baseballrocks@gmail.com>
2020-09-29 17:46:09 -0700

    1) Updated Navigation Bar 2) New navigation top app bar 3) New navigation side bar 4) Modular components such as ProfileModal 5) Updated Routes 6) New styling && Documentations completed

--- packages/AppContainer/src/App.js
+++ packages/AppContainer/src/App.js
@@ -1,9 +1,21 @@
+<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Header from './Components/Header/Header';
 import MicroserviceLoader from './MicroserviceLoader';
 import SpogPage from './Components/Spog/SpogPage';
+=======
+import React, { useState } from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import PropTypes from "prop-types"
+import clsx from "clsx"
+import { useTheme, CssBaseline } from "@material-ui/core"
+import MicroserviceLoader from "./MicroserviceLoader"
+import SpogPage from "./Components/Spog/SpogPage"
+import Header from "./Components/Header/Header"
+import containerAppUseStyles from "./ContainerStyles"
+>>>>>>> 6d495af... 1) Updated Navigation Bar
 
 const {
   REACT_APP_DASHBOARD_HOST: dashboardHost,
@@ -32,6 +44,7 @@ const User = ({ history }) => (
   <MicroserviceLoader history={history} host={userHost} name='User' />
 );
 
+<<<<<<< HEAD
 const App = () => (
   <BrowserRouter>
     <>
@@ -54,6 +67,52 @@ const App = () => (
     </>
   </BrowserRouter>
 );
+=======
+const App = () => {
+  // Styles for Container application
+  const topContainerClasses = containerAppUseStyles()
+  // Theme for container application
+  const topContainerTheme = useTheme()
+
+  // State to determine whether side navigation is open or not
+  const [sideNavOpen, setSideNavOpen] = useState(false)
+
+  return (
+    <BrowserRouter>
+      <div className={topContainerClasses.topContainerClassesRoot}>
+        <CssBaseline />
+        <Header
+          topContainerClasses={topContainerClasses}
+          topContainerTheme={topContainerTheme}
+          sideNavOpen={sideNavOpen}
+          setSideNavOpen={setSideNavOpen}
+        />
+        <main
+          className={clsx(topContainerClasses.microServiceContent, {
+            [topContainerClasses.microServiceContentShift]: sideNavOpen,
+          })}
+        >
+          <div className={topContainerClasses.sideNavDrawerHeader} />
+          <Switch>
+            <Route exact path="/" component={SpogPage} />
+            <Route exact path="/home" component={Dashboard} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/company" component={CompanyView} />
+            <Route exact path="/entity" component={Dashboard} />
+            <Route exact path="/loan" component={Dashboard} />
+            <Route exact path="/regulatory" component={Dashboard} />
+            <Route exact path="/myrequest" component={Dashboard} />
+            <Route exact path="/users" component={User} />
+            <Route exact path="/users/new" component={User} />
+            <Route exact path="/users/edit/:id" component={User} />
+            <Route exact path="/users/delete/:id" component={User} />
+          </Switch>
+        </main>
+      </div>
+    </BrowserRouter>
+  )
+}
+>>>>>>> 6d495af... 1) Updated Navigation Bar
 
 Dashboard.propTypes = {
   history: PropTypes.oneOfType([PropTypes.object]).isRequired,
