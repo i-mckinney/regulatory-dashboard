dea7bccadece27cba5cba6677042689fc199fb42
Eric <ejho13@berkeley.edu>
2020-09-30 12:35:14 -0700

    Valid indentation || Merged develop into this branch

--- packages/CompanyBackend/helpers/customAPIrequest.js
+++ packages/CompanyBackend/helpers/customAPIrequest.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+
 /**
  *
  * @param {object} customAPI
@@ -18,32 +19,45 @@ const axios = require("axios");
  * @return data you get back from the api request
  */
 async function customAPIrequest(customAPI) {
-//checking request method types
-let acceptedAPIcalls = ["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"]
-if (!acceptedAPIcalls.includes(customAPI.requestMethod.toUpperCase())){
-  throw new Error("Must input a valid type of HTTP methods")
-}
+  //checking request method types to make sure it is one of the valid request methods.
+  let acceptedAPIcalls = [
+    "GET",
+    "HEAD",
+    "POST",
+    "PUT",
+    "DELETE",
+    "CONNECT",
+    "OPTIONS",
+    "TRACE",
+    "PATCH",
+  ];
+  if (!acceptedAPIcalls.includes(customAPI.requestMethod.toUpperCase())) {
+    throw new Error("Must input a valid type of HTTP methods");
+  }
 
-if (customAPI.requestUrl.includes("http://") || customAPI.requestUrl.includes("https://")){
-  const result = axios({
-    method: customAPI.requestMethod,
-    url: customAPI.requestUrl,
-    data: customAPI.requestBody,
-    headers: customAPI.requestHeaders,
-    params: customAPI.requestParams,
-  })
-    .then((response) => {
-      console.log(response)
-      return response.data;
+  if (
+    customAPI.requestUrl.includes("http://") ||
+    customAPI.requestUrl.includes("https://")
+  ) {
+    const result = axios({
+      method: customAPI.requestMethod,
+      url: customAPI.requestUrl,
+      data: customAPI.requestBody,
+      headers: customAPI.requestHeaders,
+      params: customAPI.requestParams,
     })
-    .catch(function (error) {
-      // handle error
-      throw new Error(error);
-    });
-    return result
-  } 
-  //checking request url
-  throw new Error("http:// or https:// must be included in requestUrl")
+      .then((response) => {
+        console.log(response);
+        return response.data;
+      })
+      .catch(function (error) {
+        // handle error
+        throw new Error(error);
+      });
+    return result;
+  }
+  //checking request url if the url does not contain http:// https://, throw new express error
+  throw new Error("http:// or https:// must be included in requestUrl");
 }
 
 module.exports = customAPIrequest;
