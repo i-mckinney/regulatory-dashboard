7f763051bc60e061495d00f2ec47edbc545cf0a7
i-mckinney <baseballrocks@gmail.com>
2020-10-08 14:06:26 -0700

    tessssssssssssssssssst

--- packages/userapp/craco.config.js
+++ packages/userapp/craco.config.js
@@ -1,12 +1,5 @@
 
 module.exports = {
-  webpack: {
-    configure: {
-      externals: [
-        //this will externalize everything which start with '@material-ui'
-        /^@material-ui\/.+$/
-      ]
-    }
-  }
+
 };
 
--- packages/userapp/src/components/App.js
+++ packages/userapp/src/components/App.js
@@ -4,20 +4,38 @@ import UserTable from './user/UserTable'
 import UserCreate from './user/UserCreate'
 import UserEdit from './user/UserEdit'
 import UserDelete from './user/UserDelete'
+import jss from 'jss'
+import { StylesProvider } from '@material-ui/core/styles'
+
+const styleNode = document.createComment('jss-insertion-point');
+document.head.insertBefore(styleNode, document.head.firstChild);
+
+const createGenerateId = () => {
+  let counter = 0
+
+  return (rule, sheet) => `userApp--${rule.key}-${counter++}`
+}
+
+jss.setup({
+  createGenerateId,
+  insertionPoint: document.getElementById('insertion-point')
+})
 
 function App() {
   return (
     <div className="ui container">
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path="/users" exact component={UserTable} />
-            <Route path="/users/new" component={UserCreate}  />
-            <Route path="/users/edit/:id" component={UserEdit} />
-            <Route path="/users/delete/:id" component={UserDelete} />
-          </Switch>
-        </div>
-    </BrowserRouter>
+      <StylesProvider jss={jss}>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/users" exact component={UserTable} />
+              <Route path="/users/new" component={UserCreate} />
+              <Route path="/users/edit/:id" component={UserEdit} />
+              <Route path="/users/delete/:id" component={UserDelete} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
   );
 }
