37955f748b6f6049e36d8c292a79d5ae188325ef
i-mckinney <baseballrocks@gmail.com>
2020-10-08 13:43:20 -0700

    :bug:

--- packages/userapp/craco.config.js
+++ packages/userapp/craco.config.js
@@ -1,17 +1,12 @@
+
 module.exports = {
-  babel: {
-    plugins: [[
-      "transform-imports",
-      {
-        "@material-ui/core": {
-          "transform": "@material-ui/core/${member}",
-          "preventFullImport": true
-        },
-        "@material-ui/icons": {
-          "transform": "@material-ui/icons/${member}",
-          "preventFullImport": true
-        }
-      }
-    ]]
-  },
+  webpack: {
+    configure: {
+      externals: [
+        //this will externalize everything which start with '@material-ui'
+        /^@material-ui\/.+$/
+      ]
+    }
+  }
 };
+
