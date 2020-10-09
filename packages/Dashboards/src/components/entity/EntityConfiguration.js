e62ba349a881ea4864f773b94ad05ed56112fd68
Raymond Huang <chialh@uci.edu>
2020-10-09 15:00:06 -0700

    :sparkles::construction:

--- packages/Dashboards/src/components/entity/EntityConfiguration.js
+++ packages/Dashboards/src/components/entity/EntityConfiguration.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 
-const EntityConfiguration = () => {
+/**
+ * @param {Object} props Using the history property to route next component with data state
+ * @return {JSX} EntityCreate site with UserForm provided for entity creation
+ * routed at /entity/new
+ */
+const EntityConfiguration = (props) => {
     return (
         <div>EntityConfiguration</div>
     )
