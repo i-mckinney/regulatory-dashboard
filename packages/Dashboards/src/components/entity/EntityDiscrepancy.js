fc633b9f481c61d2540aeb3b50b3c3801309fe07
Raymond Huang <chialh@uci.edu>
2020-10-12 11:35:22 -0700

    :fire: Remove tablediscrepancy directory and use HelixTableCell import

--- packages/Dashboards/src/components/entity/EntityDiscrepancy.js
+++ packages/Dashboards/src/components/entity/EntityDiscrepancy.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types"
 import EntityCard from "./EntityCard"
 import { detailedInfo } from "../../MockData/ReconcileDWMockData"
 import HelixTable from '../table/HelixTable'
-import HelixTableCell from './tablediscrepancy/HelixTableCell'
+import HelixTableCell from '../table/HelixTableCell'
 import { HelixButton } from 'helixmonorepo-lib'
 import entities from '../apis/entities'
 
