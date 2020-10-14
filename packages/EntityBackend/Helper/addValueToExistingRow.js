29f36b612f12346dabd0b65635b50e3be9c75c63
Eric <ejho13@berkeley.edu>
2020-10-14 15:31:12 -0700

    Broke down discrepancy report api call into several modular components & fixing minor typos & added enhanced documentations

---
+++ packages/EntityBackend/Helper/addValueToExistingRow.js
@@ -0,0 +1,73 @@
+/**
+ * @param {number} doesFieldExist if the key that we are looking for exists in resultWithMapping,
+ * we add one to doesFieldExist. Then in it's parents, when doesFieldExist is greater than 0,
+ * the current for loop is skipped and doesFieldExist gets resetted to 0.
+ * @param {obj} resultWithMapping would be the final output after
+ * aggregating/mapping all the data from multiple external sources. This * will be used to populate rows of discrepancy table
+ * ex) [
+        {
+            "key_config": {
+                "key": "relationshipName",
+                "display": "relationshipName"
+            },
+            "sourceSystem": {
+                "source": "FIS",
+                "trueValue": "Rohan LLC"
+            },
+            "values": [
+                {
+                    "value": "Rohan LLC",
+                    "matchesSoT": true
+                },
+                {
+                    "value": "Rohan LLC",
+                    "matchesSoT": true
+                },
+                null
+            ]
+        },
+        {
+            "key_config": {
+                "key": "hqCountry",
+                "display": "hqCountry"
+            },
+            "sourceSystem": {
+                "source": "FIS",
+                "trueValue": "Djibouti"
+            },
+            "values": [
+                {
+                    "value": "Djibouti",
+                    "matchesSoT": true
+                },
+                null,
+                null
+            ]
+        },
+      ]
+ * 
+ * @param {string} desiredValueFromExternal value that newMappedKey will have, using response mapper from it's parents,
+ * we grab the correct value from the data we get back from external sources.
+ * @param {string} newMappedKey this will be new mapped key that would be shown in the discrepancy report
+ */
+function addValueToExistingRow(
+  doesFieldExist,
+  resultWithMapping,
+  desiredValueFromExternal,
+  newMappedKey
+) {
+
+  resultWithMapping.forEach((responseMapped) => {
+    if (responseMapped.key_config.key === newMappedKey) {
+      let sourceOfTruth =
+        desiredValueFromExternal === responseMapped.sourceSystem.trueValue;
+
+      responseMapped.values.push({
+        value: desiredValueFromExternal,
+        matchesSoT: sourceOfTruth,
+      });
+      doesFieldExist++;
+    }
+  });
+}
+module.exports = addValueToExistingRow;
--- packages/EntityBackend/routes/discrepanciesRouter.js
+++ packages/EntityBackend/routes/discrepanciesRouter.js
@@ -1,10 +1,8 @@
 // import modules
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
-
-// db setup
-const DbConnection = require("../db");
+const getEntityConfigurations = require("../Helper/getEntityConfigurations");
+const responseMapper = require("../Helper/responseMapper");
 
 //Used to get a aggregated discrepancy report for an entity
 router.get("/:companyId/:borrowerId/report", async (req, res) => {
@@ -16,125 +14,53 @@ router.get("/:companyId/:borrowerId/report", async (req, res) => {
     const companyId = req.params.companyId;
     const BorrowerId = req.params.borrowerId;
 
-    /** Using this information, we would know which custom api calls to dispatch for a discrepancy report. */
-    let entityConfigurations = axios({
-      method: "GET",
-      url: `http://localhost:4005/config/${companyId}`,
-    });
-
-    let configuredApiCalls = await entityConfigurations.then((response) => {
-      let entityConfigs = response.data.entityConfiguration;
-      return entityConfigs;
-    });
+    /** Using this information, we would know which custom api calls to dispatch for a discrepancy report. 
+     *  getEntityConfigurations(companyId) will return a list of custom api calls that has been selected in entity configuration page.
+     * ex) [{responsType:"GET", responseURL:"string", responseMapper ...}, {responseType:"GET"...}, ...]
+    */
+    let configuredApiCalls = await getEntityConfigurations(companyId);
 
     /** resultWithMapping : would be the final output after aggregating/mapping all the data from multiple external sources  */
     let resultWithMapping = [];
 
-    /** list of all new keys used in resultWithMapping */
+    /** object of all external systems used and field name after looping through all calls */
     let allNewMappedKeys = {};
 
-    /** list of all External Sources that was used */
+    /** list of headers to populate headers of a discrepancy table  */
     let TableHeaders = [{ Label: "Field Name", Accessor: "FieldName" }];
 
-    for (let i = 0; i < configuredApiCalls.length; i++) {
-      /**customAPI will be an object that contains all the necessary information to make an axios request */
-      let customAPI = configuredApiCalls[i];
-
-      const customAPIrequest = await axios({
-        method: customAPI.requestType,
-        url: customAPI.requestUrl + `/${BorrowerId}`,
-        data: customAPI.requestBody,
-        headers: customAPI.requestHeaders,
-        params: customAPI.requestParams,
-      }).then((response) => {
-        const resultData = response.data;
-        TableHeaders.push({
-          Label: response.data.ExternalSource,
-          Accessor: response.data.ExternalSource,
-        });
-
-        //In the case wehre response Mapper is given
-        if (customAPI.responseMapper) {
-          const responseMapper = customAPI.responseMapper;
-          const mappedKeyCheckOff = { ...allNewMappedKeys };
-
-          for (const externalSystemKey in responseMapper) {
-            //newMappedKey will be a new key that would be used in discrepancyReport
-            const newMappedKey = responseMapper[externalSystemKey];
-
-            if (!allNewMappedKeys.hasOwnProperty(newMappedKey)) {
-              allNewMappedKeys[newMappedKey] = "";
-            } else {
-              delete mappedKeyCheckOff[newMappedKey];
+    for (
+      let configuredApiIdx = 0;
+      configuredApiIdx < configuredApiCalls.length;
+      configuredApiIdx++
+    ) {
+      /** customAPI will be an object that contains all the necessary information to make an axios request 
+       * ex) 
+       *  {
+            "_id": "5f868ef929b51262017702fe",
+            "company_id": "5f7e1bb2ab26a664b6e950c8",
+            "requestName": "Test#1_FIS",
+            "requestUrl": "http://localhost:4005/external/FIS",
+            "responseMapper": {
+                "RelationshipName": "relationshipName",
+                "HQCountry": "hqCountry",
+                "MasterID": "masterId",
+                "HQPostalCode": "hqPostalCode",
+                "AccountTwo": "account_two"
             }
-            //value that newMappedKey will have
-            const desiredValueFromExternal = resultData[externalSystemKey];
-
-            if (!desiredValueFromExternal) {
-              console.log(
-                `External System key "${externalSystemKey}" does not exist in the external source data`
-              );
-            }
-
-            /** Case 1) newMappedKey does already exist in resultWithMapping */
-            let doesFieldExist = 0;
-
-            resultWithMapping.forEach((responseMapped) => {
-              if (responseMapped.key_config.key === newMappedKey) {
-                let sorceOfTruth =
-                  desiredValueFromExternal ===
-                  responseMapped.sourceSystem.trueValue;
-
-                responseMapped.values.push({
-                  value: desiredValueFromExternal,
-                  matchesSoT: sorceOfTruth,
-                });
-                doesFieldExist++;
-              }
-            });
-            if (doesFieldExist > 0) {
-              continue;
-            }
-
-            /** Case 2) newMappedKey does not exist in resultWithMapping; we create a new row in discrepancy report */
-
-            let valueArray = [];
-            /** We make sure that responses get mapped to a correct cell */
-            valueArray[i] = {
-              value: desiredValueFromExternal,
-              matchesSoT: true,
-            };
-            resultWithMapping.push({
-              key_config: {
-                key: newMappedKey,
-                display: newMappedKey.replace(/([A-Z])([A-Z])/g, "$1 $2"),
-              },
-              sourceSystem: {
-                source: resultData.ExternalSource,
-                trueValue: desiredValueFromExternal,
-              },
-              values: valueArray,
-            });
-          }
-
-          /**Case 3) After looping through a response mapper, we also need to check if value for previous
-           * newMappedkeys exist in the data we got back from an external source
-           */
-          for (let remainingKey in mappedKeyCheckOff) {
-            for (let i = 0; i < resultWithMapping.length; i++) {
-              if (resultWithMapping[i].key_config.key === remainingKey) {
-                resultWithMapping[i].values.push(null);
-                break;
-              }
-            }
-            resultWithMapping[i].values.push;
-          }
-        }
-      });
-      if (i === configuredApiCalls.length - 1) {
-        res.json({ TableHeaders, TableData: resultWithMapping });
-      }
+         }
+      */
+      let customAPI = configuredApiCalls[configuredApiIdx];
+      await responseMapper(
+        customAPI,
+        resultWithMapping,
+        allNewMappedKeys,
+        TableHeaders,
+        BorrowerId,
+        configuredApiIdx
+      );
     }
+    res.json({ TableHeaders, TableData: resultWithMapping });
   } catch (err) {
     res.json({ ErrorStatus: err.status, ErrorMessage: err.message });
   }
