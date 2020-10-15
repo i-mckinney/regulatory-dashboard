9ed871138e3f8830546f4209b9a1b62118c5cd5e
i-mckinney <baseballrocks@gmail.com>
2020-10-15 16:16:12 -0700

    pushed app container style provider

--- packages/AppContainer/src/App.js
+++ packages/AppContainer/src/App.js
@@ -2,7 +2,12 @@ import React, { useState } from "react"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import PropTypes from "prop-types"
 import clsx from "clsx"
-import { useTheme, CssBaseline } from "@material-ui/core"
+import {
+  useTheme,
+  CssBaseline,
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core"
 import MicroserviceLoader from "./MicroserviceLoader"
 import Header from "./Components/Header/Header"
 import containerAppUseStyles from "./ContainerStyles"
@@ -12,27 +17,31 @@ const {
   REACT_APP_COMPANY_VIEW_HOST: companyViewHost,
   REACT_APP_USER_HOST: userHost,
   REACT_APP_LOGIN_HOST: loginViewHost,
-} = process.env;
+} = process.env
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "appcontainer-",
+})
 
 const Dashboard = ({ history }) => (
-  <MicroserviceLoader history={history} host={dashboardHost} name='Dashboard' />
-);
+  <MicroserviceLoader history={history} host={dashboardHost} name="Dashboard" />
+)
 
 const CompanyView = ({ history }) => (
   <MicroserviceLoader
     history={history}
     host={companyViewHost}
-    name='CompanyView'
+    name="CompanyView"
   />
-);
+)
 
 const LoginView = ({ history }) => (
-  <MicroserviceLoader history={history} host={loginViewHost} name='LoginView' />
-);
+  <MicroserviceLoader history={history} host={loginViewHost} name="LoginView" />
+)
 
 const User = ({ history }) => (
-  <MicroserviceLoader history={history} host={userHost} name='User' />
-);
+  <MicroserviceLoader history={history} host={userHost} name="User" />
+)
 
 const App = () => {
   // Styles for Container application
@@ -44,54 +53,60 @@ const App = () => {
   const [sideNavOpen, setSideNavOpen] = useState(false)
 
   return (
-    <BrowserRouter>
-      <div className={topContainerClasses.topContainerClassesRoot}>
-        <CssBaseline />
-        <Header
-          topContainerClasses={topContainerClasses}
-          topContainerTheme={topContainerTheme}
-          sideNavOpen={sideNavOpen}
-          setSideNavOpen={setSideNavOpen}
-        />
-        <main
-          className={clsx(topContainerClasses.microServiceContent, {
-            [topContainerClasses.microServiceContentShift]: sideNavOpen,
-          })}
-        >
-          <div className={topContainerClasses.sideNavDrawerHeader} />
-          <Switch>
-            <Route exact path='/' component={LoginView} />
-            <Route exact path="/home" component={Dashboard} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/company" component={CompanyView} />
-            <Route exact path="/entity" component={Dashboard} />
-            <Route exact path="/entity/new" component={Dashboard} />
-            <Route exact path="/entity/configuration" component={Dashboard} />
-            <Route exact path="/entity/:id/discrepancy-report" component={Dashboard} />
-            <Route exact path="/entity/edit/:id" component={Dashboard} />
-            <Route exact path="/entity/delete/:id" component={Dashboard} />
-            <Route exact path="/loan" component={Dashboard} />
-            <Route exact path="/regulatory" component={Dashboard} />
-            <Route exact path="/myrequest" component={Dashboard} />
-            <Route exact path="/users" component={User} />
-            <Route exact path="/users/new" component={User} />
-            <Route exact path="/users/edit/:id" component={User} />
-            <Route exact path="/users/delete/:id" component={User} />
-          </Switch>
-        </main>
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div className={topContainerClasses.topContainerClassesRoot}>
+          <CssBaseline />
+          <Header
+            topContainerClasses={topContainerClasses}
+            topContainerTheme={topContainerTheme}
+            sideNavOpen={sideNavOpen}
+            setSideNavOpen={setSideNavOpen}
+          />
+          <main
+            className={clsx(topContainerClasses.microServiceContent, {
+              [topContainerClasses.microServiceContentShift]: sideNavOpen,
+            })}
+          >
+            <div className={topContainerClasses.sideNavDrawerHeader} />
+            <Switch>
+              <Route exact path="/" component={LoginView} />
+              <Route exact path="/home" component={Dashboard} />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/company" component={CompanyView} />
+              <Route exact path="/entity" component={Dashboard} />
+              <Route exact path="/entity/new" component={Dashboard} />
+              <Route exact path="/entity/configuration" component={Dashboard} />
+              <Route
+                exact
+                path="/entity/:id/discrepancy-report"
+                component={Dashboard}
+              />
+              <Route exact path="/entity/edit/:id" component={Dashboard} />
+              <Route exact path="/entity/delete/:id" component={Dashboard} />
+              <Route exact path="/loan" component={Dashboard} />
+              <Route exact path="/regulatory" component={Dashboard} />
+              <Route exact path="/myrequest" component={Dashboard} />
+              <Route exact path="/users" component={User} />
+              <Route exact path="/users/new" component={User} />
+              <Route exact path="/users/edit/:id" component={User} />
+              <Route exact path="/users/delete/:id" component={User} />
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
 
 Dashboard.propTypes = {
   history: PropTypes.oneOfType([PropTypes.object]).isRequired,
-};
+}
 CompanyView.propTypes = {
   history: PropTypes.oneOfType([PropTypes.object]).isRequired,
-};
+}
 User.propTypes = {
   history: PropTypes.oneOfType([PropTypes.object]).isRequired,
-};
+}
 
-export default App;
+export default App
