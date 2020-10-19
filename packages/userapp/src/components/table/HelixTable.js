2749b81e523330cb40ad9a4b5a74983b220e9168
Raymond Huang <chialh@uci.edu>
2020-10-19 13:56:28 -0700

    Add StylesProvider into HelixTable

--- packages/userapp/src/components/table/HelixTable.js
+++ packages/userapp/src/components/table/HelixTable.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react"
-import { makeStyles, Paper, TableContainer, Table } from "@material-ui/core"
+import { StylesProvider, createGenerateClassName, makeStyles, Paper, TableContainer, Table } from "@material-ui/core"
 import PropTypes from "prop-types"
 import HelixTableHead from "./HelixTableHead"
 import HelixTableBody from "./HelixTableBody"
 import HelixTableFooter from "./HelixTableFooter"
 import HelixToolBarSearch from "./HelixToolBarSearch"
 import { getComparator, stableSort } from './HelixTableSortFunc'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'helixtable-',
+})
 
 // Styling used for MaterialUI
 const helixTableStyles = makeStyles(() => ({
@@ -157,27 +161,29 @@ const HelixTable = ({
   }
   
   return (
-    <div className={helixTableClasses.helixTable}>
-    {toggleSearch ?
-      <>
-      <HelixToolBarSearch onSearch={onSearch} displayCreateIcon={displayCreateIcon} />
-      <TableContainer component={Paper}>
-        <Table aria-label="table">
-          <HelixTableHead toggleSearch={toggleSearch} order={order} orderBy={orderBy} onSort={onSort} columns={columns} customHeadColumnKeyProp={customHeadColumnKeyProp}/>
-          <HelixTableBody toggleSearch={toggleSearch} searchFilter={searchFilter} order={order} orderBy={orderBy} getComparator={getComparator} stableSort={stableSort} columns={columns} rows={rows} rowsPerPage={rowsPerPage} page={page} customCellRender={customCellRender} customBodyRowKeyProp={customBodyRowKeyProp}/>
-          <HelixTableFooter rows={rows} colSpan={columns.length} rowsPerPage={rowsPerPage} page={page} handleChangePage={handleChangePage} handleChangeRowsPerPage={handleChangeRowsPerPage} />
-        </Table>
-      </TableContainer>
-      </>
-      :
-      <TableContainer component={Paper}>
-        <Table aria-label="table">
-          <HelixTableHead onSort={onSort} columns={columns} customHeadColumnKeyProp={customHeadColumnKeyProp}/>
-          <HelixTableBody searchFilter={searchFilter} getComparator={getComparator} stableSort={stableSort} columns={columns} rows={rows} customCellRender={customCellRender} customBodyRowKeyProp={customBodyRowKeyProp}/>
-        </Table>
-      </TableContainer>
-    }
-    </div>
+    <StylesProvider generateClassName={generateClassName} injectFirst>
+      <div className={helixTableClasses.helixTable}>
+      {toggleSearch ?
+        <>
+        <HelixToolBarSearch onSearch={onSearch} displayCreateIcon={displayCreateIcon} />
+        <TableContainer component={Paper}>
+          <Table aria-label="table">
+            <HelixTableHead toggleSearch={toggleSearch} order={order} orderBy={orderBy} onSort={onSort} columns={columns} customHeadColumnKeyProp={customHeadColumnKeyProp}/>
+            <HelixTableBody toggleSearch={toggleSearch} searchFilter={searchFilter} order={order} orderBy={orderBy} getComparator={getComparator} stableSort={stableSort} columns={columns} rows={rows} rowsPerPage={rowsPerPage} page={page} customCellRender={customCellRender} customBodyRowKeyProp={customBodyRowKeyProp}/>
+            <HelixTableFooter rows={rows} colSpan={columns.length} rowsPerPage={rowsPerPage} page={page} handleChangePage={handleChangePage} handleChangeRowsPerPage={handleChangeRowsPerPage} />
+          </Table>
+        </TableContainer>
+        </>
+        :
+        <TableContainer component={Paper}>
+          <Table aria-label="table">
+            <HelixTableHead onSort={onSort} columns={columns} customHeadColumnKeyProp={customHeadColumnKeyProp}/>
+            <HelixTableBody searchFilter={searchFilter} getComparator={getComparator} stableSort={stableSort} columns={columns} rows={rows} customCellRender={customCellRender} customBodyRowKeyProp={customBodyRowKeyProp}/>
+          </Table>
+        </TableContainer>
+      }
+      </div>
+    </StylesProvider>
   )
 }
 
