e900ada53769c0ff7e1598aac0fab18e92478a8c
Taharka Taylor <taharka.taylor@helixcp.com>
2020-10-19 14:40:07 -0700

    commit

--- packages/CompanyPortal/src/components/api-table/PerformTestDialog.js
+++ packages/CompanyPortal/src/components/api-table/PerformTestDialog.js
@@ -1,50 +1,98 @@
 import React, { useState, useEffect } from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
+// import CircularProgress from '@material-ui/core/CircularProgress';
+import { TextField, Button, InputBase } from '@material-ui/core';
 import axios from 'axios';
 import DialogModalTemplate from '../DialogModalTemplate';
 import Grid from '@material-ui/core/Grid';
 
-const PerformTestDialog = ({ open, onClose, requestData: { requestName, _id : requestId } }) => {
+const PerformTestDialog = ({
+  open,
+  onClose,
+  requestData: { requestName, _id: requestId },
+}) => {
+  console.log(requestId);
   const [testResponse, setTestResponse] = useState(null);
+  const [response, setResponse] = useState(null);
+  const [mappedResponse, setMappedResponse] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const testRequest = async () => {
-      setLoading(true);
-      //const response = await axios.get(getTestUrl(requestId))
-      const response = await axios.get('http://localhost:5000/companies');
-      setTestResponse(response.data);
-      setLoading(false);
-    };
-    if (open) {
-      testRequest();
-    }
-  }, [requestId, open]);
+  const [inputState, setInputState] = useState('');
+
+  const testRequest = async (borrowerId) => {
+    // setLoading(true);
+    //const response = await axios.get(getTestUrl(requestId))
+    const response = await axios.get(
+      `http://localhost:5000/companies/5f7e1bb2ab26a664b6e950c8/customapi/${requestId}/test/${borrowerId}`
+    );
+    setTestResponse(response.data);
+    setResponse(response.data.externalSourceData);
+    setMappedResponse(response.data.responseMapped);
+    // setLoading(false);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    testRequest(inputState);
+  };
+  const handleChange = (evt) => {
+    setInputState(evt.target.value);
+  };
+
+  // useEffect(() => {
+  //   const testRequest = async () => {
+  //     setLoading(true);
+  //     //const response = await axios.get(getTestUrl(requestId))
+  //     const response = await axios.get(
+  //       `http://localhost:5000/companies/5f7e1bb2ab26a664b6e950c8/customapi/${requestId}/test`
+  //     );
+  //     setTestResponse(response.data);
+  //     setResponse(response.data.externalSourceData);
+  //     setMappedResponse(response.data.responseMapped);
+  //     setLoading(false);
+  //   };
+  //   if (open) {
+  //     testRequest();
+  //   }
+  // }, [requestId, open]);
 
   if (!open) {
     return null;
   }
+
   return (
     <DialogModalTemplate
-      title='API Response:'
+      title='Test API Response'
       open={open}
       onClose={onClose}
     >
+      {/* <form noValidate autoComplete="off" onSubmit={handleSubmit}> */}
+      <div style={{ display: 'flex', alignItems: 'baseline' }}>
+        <TextField
+          id='standard-basic'
+          label='Borrower Id'
+          onChange={handleChange}
+        />
+        <Button
+          style={{ marginLeft: '10px' }}
+          variant='contained'
+          color='secondary'
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
+      </div>
+      {/* </form> */}
       <h3>Testing API Call "{`${requestName}`}"</h3>
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        <Grid container spacing={2}>
-          <Grid item md={6} style={{ borderRight: "1px solid gray" }}>
-            <h4>Response</h4>
-            <pre>{JSON.stringify(testResponse, null, 2)}</pre>
-          </Grid>
-          <Grid item md={6}>
-            <h4>Mapped Response</h4>
-            <pre>{JSON.stringify(testResponse && testResponse.mappedResponse, null, 2)}</pre>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid item md={6} style={{ borderRight: '1px solid gray' }}>
+          <h4>Response</h4>
+          <pre>{response ? JSON.stringify(response, null, 2) : null}</pre>
         </Grid>
-      )}
+        <Grid item md={6}>
+          <h4>Mapped Response</h4>
+          <pre>{response ? JSON.stringify(mappedResponse, null, 2) : null}</pre>
+        </Grid>
+      </Grid>
     </DialogModalTemplate>
   );
 };
