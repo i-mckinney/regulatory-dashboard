153da353db5f714c1a71b7e0788a4f91580f258b
Raymond Huang <chialh@uci.edu>
2020-10-21 13:48:34 -0700

    :alien:

--- packages/Dashboards/src/components/apis/companies.js
+++ packages/Dashboards/src/components/apis/companies.js
@@ -1,11 +1,10 @@
 import axios from "axios"
 
 const {
-    REACT_APP_COMPANIES_HOST: companiesBackEndHost,
-    REACT_APP_COMPANIES_PORT: companiesBackEndPort,
+    REACT_APP_API_GATEWAY_HOST: companiesURL,
 } = process.env
 
 // Set config defaults when creating the instance
 export default axios.create({
-  baseURL: `http://${companiesBackEndHost}:${companiesBackEndPort}`,
+  baseURL: `http://${companiesURL}`,
 });
