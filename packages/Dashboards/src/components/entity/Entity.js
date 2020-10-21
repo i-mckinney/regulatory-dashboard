72b69def66a2ad421656aaa6602baf9d3c52e7a2
Raymond Huang <chialh@uci.edu>
2020-10-20 18:43:12 -0700

    :memo:

--- packages/Dashboards/src/components/entity/Entity.js
+++ packages/Dashboards/src/components/entity/Entity.js
@@ -8,7 +8,6 @@ import AssessmentIcon from '@material-ui/icons/Assessment'
 import EditIcon from '@material-ui/icons/Edit'
 import DeleteIcon from '@material-ui/icons/Delete'
 import { HelixTable, HelixTableCell } from 'helixmonorepo-lib'
-
 import entities from '../apis/entities'
 import { sortableExcludes, columnExcludes, columnLabels } from '../../config'
 
