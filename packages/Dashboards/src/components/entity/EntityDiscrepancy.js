c451c988959fff4dd73c53510670592cba21d9a5
Raymond Huang <chialh@uci.edu>
2020-10-22 15:32:04 -0700

    Add HelixTable and HelixTableCell imports back

--- packages/Dashboards/src/components/entity/EntityDiscrepancy.js
+++ packages/Dashboards/src/components/entity/EntityDiscrepancy.js
@@ -5,9 +5,7 @@ import { Alert, AlertTitle } from '@material-ui/lab'
 import PropTypes from "prop-types"
 import EntityCard from "./EntityCard"
 import { detailedInfo } from "../../MockData/ReconcileDWMockData"
-import { HelixButton } from 'helixmonorepo-lib'
-import HelixTable from '../table/HelixTable'
-import HelixTableCell from '../table/HelixTableCell'
+import { HelixTable, HelixTableCell, HelixButton } from 'helixmonorepo-lib'
 import entities from '../apis/entities'
 
 // Styling used for MaterialUI
