07afebeeda6ef2b9733164c691908d9cbe98fadc
Raymond Huang <chialh@uci.edu>
2020-11-05 14:49:49 -0800

    Add functionality into HelixCard

--- packages/Dashboards/src/components/report/Report.js
+++ packages/Dashboards/src/components/report/Report.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import HelixCard from '../utils/HelixCard'
 
 /** @return {JSX} Report site
  * routed at /Report
  */
 
 function Report() {
-    return <div>Report</div>
+    return <div><HelixCard /></div>
 }
 
 export default Report
--- packages/Dashboards/src/components/utils/HelixCard.js
+++ packages/Dashboards/src/components/utils/HelixCard.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import { red } from '@material-ui/core/colors'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 
-/** @return {JSX} Report site
- * routed at /Report
- */
+const helixCardStyles = makeStyles(() => ({
+    root: {
+      maxWidth: 345,
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }))
 
 const HelixCard = () => {
-    return <div>HelixCard</div>
+    const helixCardclasses = helixCardStyles();
+
+    return (
+    <Card className={helixCardclasses.root}>
+        <CardHeader
+            avatar={
+            <Avatar aria-label="recipe" className={helixCardclasses.avatar}>
+                R
+            </Avatar>
+            }
+            action={
+            <IconButton aria-label="settings">
+                <MoreVertIcon />
+            </IconButton>
+            }
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
+        />
+        <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+            This impressive paella is a perfect party dish and a fun meal to cook together with your
+            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+            </Typography>
+        </CardContent>
+    </Card>
+    )
 }
 
 export default HelixCard
