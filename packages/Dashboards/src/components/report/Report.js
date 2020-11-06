29608a2a0e111e2f6abeba66eef6bcd8c28ca3e5
Raymond Huang <chialh@uci.edu>
2020-11-05 18:34:31 -0800

    Set up skeleton for Report outlook and search bar with add icon

--- packages/Dashboards/src/components/report/Report.js
+++ packages/Dashboards/src/components/report/Report.js
@@ -1,12 +1,57 @@
 import React from 'react'
+import { makeStyles, Typography, IconButton } from '@material-ui/core'
 import HelixCard from '../utils/HelixCard'
+import HelixToolBarSearch from '../table/HelixToolBarSearch'
+import AddBoxIcon from '@material-ui/icons/AddBox'
+
+// Styling used for MaterialUI
+const reportStyles = makeStyles(() => ({
+    mediumContainer: {
+        width: '80%',
+        margin: 'auto',
+        marginTop: '3rem',
+        paddingBottom: '3rem',
+    },
+    createIconStyle: {
+        float: 'right',
+        cursor: 'pointer',
+        marginLeft: "auto",
+    },
+    header: {
+        paddingBottom: '2rem',
+    },
+  }))
 
 /** @return {JSX} Report site
  * routed at /Report
  */
-
 function Report() {
-    return <div><HelixCard /></div>
+    const reportClasses = reportStyles()
+
+    /**
+     * @return jsx object of create icon in child component's toolbar
+     */
+    const displayCreateReportIcon = () => {
+        return (
+          <span className={reportClasses.createIconStyle}>
+            <IconButton
+            color="primary"
+            >
+                <AddBoxIcon fontSize="large" />
+            </IconButton>
+          </span>
+        )
+    }
+
+    return (
+        <div className={reportClasses.mediumContainer}>
+            <div className={reportClasses.header}>
+                <Typography variant="h5">Choose Your Report</Typography>
+            </div>
+            <HelixToolBarSearch displayCreateIcon={displayCreateReportIcon} />
+            
+        </div>
+    )
 }
 
 export default Report
