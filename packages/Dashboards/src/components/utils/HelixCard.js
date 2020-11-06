50a4362a9d584655e30e76ce0fbfc1ea8419090d
Raymond Huang <chialh@uci.edu>
2020-11-06 00:18:46 -0800

    :lipstick: Add hover background color/highlights

--- packages/Dashboards/src/components/utils/HelixCard.js
+++ packages/Dashboards/src/components/utils/HelixCard.js
@@ -14,7 +14,11 @@ const helixCardStyles = makeStyles(() => ({
       marginTop: '25px',
       marginLeft: '25px',
       marginRight: '40px',
-      boxShadow: '0px 6px 6px -3px rgba(0,0,0,0.2), 0px 10px 14px 1px rgba(0,0,0,0.14), 0px 4px 18px 3px rgba(0,0,0,0.12)'
+      boxShadow: '0px 6px 6px -3px rgba(0,0,0,0.2), 0px 10px 14px 1px rgba(0,0,0,0.14), 0px 4px 18px 3px rgba(0,0,0,0.12)',
+      cursor: 'pointer',
+      '&:hover': {
+        backgroundColor: '#e0e0e0'
+      }
     },
     avatar: {
       backgroundColor: red[500],
