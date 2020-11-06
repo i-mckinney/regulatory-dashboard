53c4704a11df3dc01a1cde74ba1e43e7ac484978
Raymond Huang <chialh@uci.edu>
2020-11-06 15:23:04 -0800

    Add HelixCollectionList reusable component

---
+++ packages/Dashboards/src/components/utils/HelixCollectionList.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { makeStyles, Paper, GridList, GridListTile } from '@material-ui/core'
+import PropTypes from 'prop-types'
+
+const helixCollectionListStyles = makeStyles(() => ({
+    paper: {
+        marginTop: '1rem',
+    },
+    gridList: {
+        width: '100%',
+        height: 500,
+        transform: 'translateZ(0)',
+    },
+}))
+
+const HelixCollectionList = () => {
+    // Creates an object for styling. Any className that matches key in the helixCollectionListStyles object will have a corresponding styling
+    const helixCollectionListClasses = helixCollectionListStyles()
+
+    return (
+    <Paper elevation={props.elevation} className={helixCollectionListClasses.paper}>
+        <GridList cellHeight={props.cellHeight} spacing={props.spacing} cols={props.columnSpan} className={helixCollectionListClasses.gridList}>
+            {props.reportData.map((report) => (
+                <GridListTile key={report._id}>
+                    <HelixCard 
+                    user={props.user} 
+                    lastModifiedBy={report.lastModifiedBy} 
+                    createdAt={report.createdAt} 
+                    handleEditReport={props.handleEditReport}
+                    handleDeleteReport={props.handleDeleteReport}
+                    />
+                </GridListTile>
+            ))}
+        </GridList>
+    </Paper>
+    )
+}
+
+HelixCollectionList.propTypes = {
+    user: PropTypes.string.isRequired,
+    reportData: PropTypes.instanceOf(Array).isRequired,
+    elevation: PropTypes.number.isRequired,
+    cellHeight: PropTypes.number.isRequired,
+    spacing: PropTypes.number.isRequired,
+    columnSpan: PropTypes.number.isRequired,
+    handleEditReport: PropTypes.func.isRequired,
+    handleDeleteReport: PropTypes.func.isRequired,
+}
+
+HelixCollectionList.defaultProps = {
+    elevation: 10,
+    cellHeight: 200,
+    spacing: 10,
+    columnSpan: 3,
+}
+
+export default HelixCollectionList
