8b6fdca667ba713a4b593c5cd519802e19977425
Raymond Huang <chialh@uci.edu>
2020-11-05 16:43:36 -0800

    Add functionality into HelixMenuIcon

--- packages/Dashboards/src/components/utils/HelixMenuIcon.js
+++ packages/Dashboards/src/components/utils/HelixMenuIcon.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
+import IconButton from '@material-ui/core/IconButton'
+import Grow from '@material-ui/core/Grow'
+import Paper from '@material-ui/core/Paper'
+import Popper from '@material-ui/core/Popper'
+import MenuItem from '@material-ui/core/MenuItem'
+import MenuList from '@material-ui/core/MenuList'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+// import { makeStyles } from '@material-ui/core'
 
 const HelixMenuIcon = () => {
     return (
+        <div>
         <IconButton aria-label="settings">
             <MoreVertIcon />
         </IconButton>
+        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleClose}>My account</MenuItem>
+                  <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+        </Popper>
+      </div>
     )
 }
 
