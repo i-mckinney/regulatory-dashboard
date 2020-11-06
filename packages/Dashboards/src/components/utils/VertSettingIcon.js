97600e4b349bffbb759068f136a074f35c4b5cea
Raymond Huang <chialh@uci.edu>
2020-11-05 16:35:56 -0800

    Add MenuSettingIcon component

---
+++ packages/Dashboards/src/components/utils/VertSettingIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VertSettingIcon = () => {
+    return (
+        <IconButton aria-label="settings">
+            <MoreVertIcon />
+        </IconButton>
+    )
+}
+
+export default VertSettingIcon
