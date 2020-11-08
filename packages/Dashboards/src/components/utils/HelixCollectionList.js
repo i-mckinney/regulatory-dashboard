db67204f5d7e3be90676bd82411314d4b25ed5ed
Raymond Huang <chialh@uci.edu>
2020-11-07 22:35:36 -0800

    :sparkles::beers::alembic::construction: Add Search Filter functionality feature thats search only one grouping

--- packages/Dashboards/src/components/utils/HelixCollectionList.js
+++ packages/Dashboards/src/components/utils/HelixCollectionList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { makeStyles, Paper, GridList, GridListTile } from '@material-ui/core'
 import HelixCard from './HelixCard'
 import PropTypes from 'prop-types'
+import { getComparator, stableSort } from '../table/HelixTableSortFunc'
 
 const helixCollectionListStyles = makeStyles(() => ({
     paper: {
@@ -18,10 +19,12 @@ const HelixCollectionList = (props) => {
     // Creates an object for styling. Any className that matches key in the helixCollectionListStyles object will have a corresponding styling
     const helixCollectionListClasses = helixCollectionListStyles()
 
+    const sortedReports = stableSort(props.searchFilter.search(props.reportData), getComparator('asc', 'createdAt'))
+
     return (
     <Paper elevation={props.elevation} className={helixCollectionListClasses.paper}>
         <GridList cellHeight={props.cellHeight} spacing={props.spacing} cols={props.columnSpan} className={helixCollectionListClasses.gridList}>
-            {props.reportData.map((report) => (
+            {sortedReports.map((report) => (
                 <GridListTile key={report._id}>
                     <HelixCard 
                     user={props.user} 
