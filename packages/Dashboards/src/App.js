180a4428fcfa525d924adfbd41c051ec40d65b22
Raymond Huang <chialh@uci.edu>
2020-11-09 14:10:10 -0800

    :fire: Add ReportCreate component and Change route pointing to ReportCreate

--- packages/Dashboards/src/App.js
+++ packages/Dashboards/src/App.js
@@ -12,7 +12,7 @@ import EntityCreate from "./components/entity/EntityCreate"
 import EntityEdit from "./components/entity/EntityEdit"
 import EntityDelete from "./components/entity/EntityDelete"
 import Report from './components/report/Report'
-import ReportInputForm from './components/report/ReportInputForm'
+import ReportCreate from './components/report/ReportCreate'
 import {
   StylesProvider,
   createGenerateClassName,
@@ -79,7 +79,7 @@ function App(history) {
             </Route>
 
             <Route exact path="/report/new">
-              <ReportInputForm />
+              <ReportCreate />
             </Route>
 
             <Redirect to="/" />
---
+++ packages/Dashboards/src/components/report/ReportCreate.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReportCreate = () => {
+    return (
+        <div>ReportCreate</div>
+    )
+}
+
+export default ReportCreate
