abd72047bffafff90253624c107c0636aae51c43
Raymond Huang <chialh@uci.edu>
2020-11-09 12:38:44 -0800

    :alembic: Experiment and Add Route for ReportInputForm which will be used for Create and Edit

--- packages/Dashboards/src/App.js
+++ packages/Dashboards/src/App.js
@@ -12,6 +12,7 @@ import EntityCreate from "./components/entity/EntityCreate"
 import EntityEdit from "./components/entity/EntityEdit"
 import EntityDelete from "./components/entity/EntityDelete"
 import Report from './components/report/Report'
+import ReportInputForm from './components/report/ReportInputForm'
 import {
   StylesProvider,
   createGenerateClassName,
@@ -75,6 +76,10 @@ function App(history) {
 
             <Route exact path="/report">
               <Report />
+            </Route>
+
+            <Route exact path="/report/new">
+              <ReportInputForm />
             </Route>
 
             <Redirect to="/" />
