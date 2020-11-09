dec3362873dac036408009308175c528aa12609e
Raymond Huang <chialh@uci.edu>
2020-11-09 15:58:21 -0800

    :wrench: Update Report env configs

--- packages/Dashboards/src/components/report/config.js
+++ packages/Dashboards/src/components/report/config.js
@@ -4,14 +4,14 @@ export const sortableExcludes = ["_id", "Actions"]
 // columnExcludes is array of identifier that we do not want to display in the table
 export const columnExcludes = []
 
-// columnFields is array of fields that UserForm will have 
-export const columnFields = ["reportName"]
+// columnFields is array of fields that ReportInputForm will have 
+export const columnFields = ["reportName", "preferences", "archive"]
 
 // columnLabels is array of label name that correspondings to columnFields EXCEPT _id (primary key)
 // PRIMARY KEY must be FIRST in the array (required)
 // These KEYS must be LAST in the array (required unless place it in columnExcludes)
-// (1) User Created ( refers to "createdAt" (mongodb semantics) )
-// (2) User Updated ( refers to replaces "updatedAt" (mongodb semantics) )
+// (1) Report Created ( refers to "createdAt" (mongodb semantics) )
+// (2) Report Updated ( refers to replaces "updatedAt" (mongodb semantics) )
 // (3) Actions
-export const columnLabels = ["_id", "Report Name"]
+export const columnLabels = ["_id", "Report Name", "Perferences", "Archive"]
 
