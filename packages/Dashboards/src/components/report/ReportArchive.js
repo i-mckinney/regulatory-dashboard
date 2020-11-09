a597b208b67a726f81dee39c8c95e3e9c5ead161
Raymond Huang <chialh@uci.edu>
2020-11-09 15:15:36 -0800

    Add base ReportArchive and ReportPreference component

---
+++ packages/Dashboards/src/components/report/ReportArchive.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReportArchive = () => {
+    return (
+        <div>ReportArchive Component</div>
+    )
+}
+
+export default ReportArchive
---
+++ packages/Dashboards/src/components/report/ReportPreference.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReportPreference = () => {
+    return (
+        <div>ReportPreference Component</div>
+    )
+}
+
+export default ReportPreference
