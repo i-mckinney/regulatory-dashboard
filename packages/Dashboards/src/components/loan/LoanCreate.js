7c1344e7a9686e6fb4fe3783486854d7a6cf3377
Raymond Huang <chialh@uci.edu>
2020-11-14 22:20:41 -0800

    :art::sparkles::tada :Add a base LoanCreate component

---
+++ packages/Dashboards/src/components/loan/LoanCreate.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+/** @return {JSX} LoanCreate site
+ * routed at /LoanCreate
+ */
+function LoanCreate() {
+  return (
+    <div>
+      LoanCreate
+    </div>
+  )
+}
+
+export default LoanCreate
