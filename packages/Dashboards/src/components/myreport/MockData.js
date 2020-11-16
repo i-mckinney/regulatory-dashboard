fd4a1030c95940023084f81b42b127a69630fe04
Raymond Huang <chialh@uci.edu>
2020-11-16 14:55:04 -0800

    :clown_face::alembic: Add mock data

---
+++ packages/Dashboards/src/components/myreport/MockData.js
@@ -0,0 +1,53 @@
+const mockData = [
+    {
+        _id: "1",
+        loanName: "Test #1",
+        loanType: "Student Loan",
+        commitmentAmount: "$123,456,789",
+        maturityDate: "2025-01-01",
+        Actions: "",
+    },
+    {
+        _id: "2",
+        loanName: "Test #2",
+        loanType: "Mortgage Loan",
+        commitmentAmount: "$900,000",
+        maturityDate: "2028-09-09",
+        Actions: "",
+    },
+    {   
+        _id: "3",
+        loanName: "Test #3",
+        loanType: "Auto Loan",
+        commitmentAmount: "$456,000",
+        maturityDate: "2030-03-28",
+        Actions: "",
+    },
+    {
+        _id: "4",
+        loanName: "Test #4",
+        loanType: "Personal Loan",
+        commitmentAmount: "$989,000",
+        maturityDate: "2024-11-11",
+        Actions: "",
+    },
+    {
+        _id: "5",
+        loanName: "Test #5",
+        loanType: "Home Equity",
+        commitmentAmount: "$1,080,000",
+        maturityDate: "2035-01-01",
+        Actions: "",
+    },
+    {
+        _id: "1",
+        loanName: "Test #6 ",
+        loanType: "Auto title Loan",
+        commitmentAmount: "Jane Doe",
+        maturityDate: "2025-12-12",
+        Actions: "",
+    },
+  
+  ]
+
+export default mockData
