14e836f9809b489fa66da9b357e958e010802a0b
Raymond Huang <chialh@uci.edu>
2020-11-15 19:48:39 -0800

    :art::sparkles: Add LoanForm component

---
+++ packages/Dashboards/src/components/loan/LoanForm.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+/** @return {JSX} LoanForm site
+ * routed at /LoanForm
+ */
+function LoanForm() {
+  return (
+    <div>
+      LoanForm
+    </div>
+  )
+}
+
+export default LoanForm
