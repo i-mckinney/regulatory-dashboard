b141c8b7e1cb32a1c09d94b7f87f01b4768c977c
Raymond Huang <chialh@uci.edu>
2020-11-15 19:47:05 -0800

    :art::sparkles: Add LoanDiscrepancy component

---
+++ packages/Dashboards/src/components/loan/LoanDiscrepancy.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+/** @return {JSX} LoanDiscrepancy site
+ * routed at /LoanDiscrepancy
+ */
+function LoanDiscrepancy() {
+  return (
+    <div>
+      LoanDiscrepancy
+    </div>
+  )
+}
+
+export default LoanDiscrepancy
