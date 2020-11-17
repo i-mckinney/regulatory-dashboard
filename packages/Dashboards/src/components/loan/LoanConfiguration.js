f3a42e2bc4e520ccef8567bb2dd25bdbe0265832
Raymond Huang <chialh@uci.edu>
2020-11-17 15:01:13 -0800

    :sparkles::boom: Add LoanConfiguration component

---
+++ packages/Dashboards/src/components/loan/LoanConfiguration.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+/** @return {JSX} LoanConfiguration site
+ * routed at /loan/configuration/:id
+ */
+function LoanConfiguration() {
+  return (
+    <div>
+      LoanConfiguration
+    </div>
+  )
+}
+
+export default LoanConfiguration
