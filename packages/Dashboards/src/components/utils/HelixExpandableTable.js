24ed9a5a6903f4f97443307f6ab00be9f6fff72b
Raymond Huang <chialh@uci.edu>
2020-11-18 15:09:30 -0800

    :art: Add base HelixExpandableTable

---
+++ packages/Dashboards/src/components/utils/HelixExpandableTable.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+/** @return {JSX} LoanForm template
+ */
+function HelixExpandedTable() {
+  return (
+    <div>
+      HelixExpandedTable
+    </div>
+  )
+}
+
+export default HelixExpandedTable
