81109271527039d57a56402d8d4e08dcfc7a7a06
Raymond Huang <chialh@uci.edu>
2020-12-07 11:47:57 -0800

    Remove unused imports

--- packages/Dashboards/src/components/report/Report.js
+++ packages/Dashboards/src/components/report/Report.js
@@ -5,7 +5,6 @@ import HelixToolBarSearch from '../table/HelixToolBarSearch'
 import AddBoxIcon from '@material-ui/icons/AddBox'
 import HelixReportCard from '../utils/HelixReportCard'
 import HelixCollectionList from '../utils/HelixCollectionList'
-import ReportNormalizationTable from './ReportNormalizationTable'
 
 // Styling used for MaterialUI
 const reportStyles = makeStyles(() => ({
@@ -232,7 +231,6 @@ function Report(props) {
             handleEditComponent={handleEditReport} 
             handleDeleteComponent={handleDeleteReport}
             />
-            <ReportNormalizationTable />
         </div>
     )
 }
