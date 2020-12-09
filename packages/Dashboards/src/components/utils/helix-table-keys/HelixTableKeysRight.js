3d70d68ed89bad6707a88e3635c8074f279e24e6
Taharka Taylor <taharka.taylor@helixcp.com>
2020-12-08 19:38:06 -0800

    dynamic cards

--- packages/Dashboards/src/components/utils/helix-table-keys/HelixTableKeysRight.js
+++ packages/Dashboards/src/components/utils/helix-table-keys/HelixTableKeysRight.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import {Container, Grid, Box, InputAdornment, Select, MenuItem, FormControl, InputLabel,} from '@material-ui/core'
 import { HelixButton, HelixTextField } from 'helixmonorepo-lib'
@@ -15,7 +15,16 @@ const useTableKeysStyles = makeStyles((theme) => ({
 
 const HelixTableKeysRight = (props) => {
   const useTableKeysClasses = useTableKeysStyles()
+  // Provides available sort methods for Select menu
   const [sortMethod, setSortMethod] = React.useState('')
+  // Provides list of mock data table keys 
+  const [ items, setItems] = useState([
+    {key: "firstName", value: 1},
+    {key: "lastName", value: 2},
+    {key: "relationshipName", value: 3},
+    {key: "accountNumber", value: 4},
+    {key: "accountID", value: 5},
+  ])
 
   const handleChange = (e) => {
     setSortMethod(e.target.value);
@@ -27,56 +36,71 @@ const HelixTableKeysRight = (props) => {
       <Grid container spacing={2} >
         <Grid item md={12}>
           <Box display='flex' p={1}>
-          <Box p={1} flexGrow={1}><div><h1>Entities Table Keys</h1><h3>Total Number of Keys: {props.quantity}</h3></div></Box>
-          <Box p={1}><HelixButton 
-              color="primary"
-              variant="contained" 
-              size="large"
-              text="Add New Key"
-              style={{marginLeft: '2em'}}
-              onClick
-            /></Box>
+            <Box p={1} flexGrow={1}>
+              <div>
+                <h1>Entities Table Keys</h1>
+                <h3>Total Number of Keys: {props.quantity}</h3>
+              </div>
             </Box>
+            <Box p={1}>
+              <HelixButton 
+                color="primary"
+                variant="contained" 
+                size="large"
+                text="Add New Key"
+                style={{marginLeft: '2em'}}
+                onClick
+              />
+            </Box>
+          </Box>
         </Grid>
         <Grid item md={12}>
           <Box display='flex' p={1}>
-          <Box p={1} flexGrow={1}><HelixTextField
-        style={{width: '25em'}}  
-        label="Search" 
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position='start'>
-              <SearchIcon />
-            </InputAdornment>
-          )
-        }}
-        /></Box>
-          <Box p={1}> <FormControl variant="filled" className={useTableKeysClasses.formControl}>
-        <InputLabel id="demo-simple-select-filled-label">Sort By</InputLabel>
-        <Select
-          labelId="demo-simple-select-filled-label"
-          id="demo-simple-select-filled"
-          value={sortMethod}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>Recently Added</em>
-          </MenuItem>
-          <MenuItem value={'Key Name'}>Key Name</MenuItem>
-          <MenuItem value={'Key Value'}>Key Value</MenuItem>
-        </Select>
-      </FormControl>
-      </Box>
-        </Box>
+            <Box p={1} flexGrow={1}>
+              <HelixTextField
+                style={{width: '25em'}}  
+                label="Search" 
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position='start'>
+                      <SearchIcon />
+                    </InputAdornment>
+                    )
+                  }}
+              />
+            </Box>
+            <Box p={1}> 
+              <FormControl variant="filled" className={useTableKeysClasses.formControl}>
+                <InputLabel id="demo-simple-select-filled-label">Sort By</InputLabel>
+                <Select
+                  labelId="demo-simple-select-filled-label"
+                  id="demo-simple-select-filled"
+                  value={sortMethod}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">
+                  <em>Recently Added</em>
+                  </MenuItem>
+                  <MenuItem value={'Key Name'}>Key Name</MenuItem>
+                  <MenuItem value={'Key Value'}>Key Value</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
         </Grid>
       </Grid>
-      <Grid container spacing={3}><Grid item md={12}><TableKeysCard/></Grid>
-      <Grid item md={12}><TableKeysCard/></Grid>
-      <Grid item md={12}><TableKeysCard/></Grid>
-      <Grid item md={12}><TableKeysCard/></Grid>
-      <Grid item md={12}><TableKeysCard/></Grid></Grid>
-      
-
+      <Grid container spacing={3}>
+        {items.map(({ key, value }) => (
+        <Grid key={key} item md={12}>
+          <TableKeysCard
+            keyName={key}
+            value={value}
+            onEdit={(newKey) => setItems(items.map(({ key: k, value: v }) => k == key ? { key: newKey, value: v } : { key: k, value: v } ))}
+            onDelete={() => setItems(items.filter(({ key: k }) => k != key))}
+            />
+        </Grid>
+          ))}
+      </Grid>
     </Container>
     </>
   )
--- packages/Dashboards/src/components/utils/helix-table-keys/TableKeysCard.js
+++ packages/Dashboards/src/components/utils/helix-table-keys/TableKeysCard.js
@@ -4,38 +4,38 @@ import {Grid, Box, Paper, IconButton} from '@material-ui/core'
 import CreateIcon from '@material-ui/icons/Create';
 import DeleteIcon from '@material-ui/icons/DeleteOutline';
 
-const TableKeysCard = () => {
+const TableKeysCard = ({ keyName, value, onEdit, onDelete }) => {
   return (
     <Box>
-    <Grid container>
-      <Grid item md={12}>
-      <Paper elevation={1}>
-        <Box display='flex' p={1}>
-          <Box p={1} flexGrow={1}><p>Key Name</p></Box>
-          <Box p={1}>
-            <IconButton
-            aria-label="delete"
-            size="medium"
-            edge="start"
-            color="primary"
-            >
-            <CreateIcon />
-
-            </IconButton>
-            <IconButton
-            aria-label="delete"
-            size="medium"
-            edge="false"
-            color="secondary"
-            >
-              <DeleteIcon />
-            </IconButton>
-
-          </Box>
-        </Box>
-      </Paper>
-      </Grid>
-    </Grid>  
+      <Grid container>
+        <Grid item md={12}>
+          <Paper elevation={1}>
+            <Box display='flex' p={1}>
+              <Box p={1} flexGrow={1}><p>{keyName}</p>
+              </Box>
+              <Box p={1}>
+                <IconButton
+                aria-label="delete"
+                size="medium"
+                edge="start"
+                color="primary"
+                >
+                  <CreateIcon />
+                </IconButton>
+                <IconButton
+                aria-label="delete"
+                size="medium"
+                edge="false"
+                color="secondary"
+                onClick={onDelete}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </Box>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>  
     </Box>
   )
 }
