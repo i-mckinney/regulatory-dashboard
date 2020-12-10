7ac99bd24734e0e714e9dda55042920d9e2a0e73
Jacob Nastaskin <jacob.nastaskin@gmail.com>
2020-12-09 18:15:52 -0800

    ✨ Update/Add API Request -> add/edit keys  in Mapping Tool Tab

--- packages/CompanyPortal/src/components/controls/Input.js
+++ packages/CompanyPortal/src/components/controls/Input.js
@@ -11,7 +11,7 @@ export default function Input(props) {
   value: the form control value
   onChange: the function called on form change detection
   */
-  const { name, label, value, onChange, width } = props;
+  const { name, label, value, onChange, width, padding, InputProps } = props;
 
   return (
     <TextField
@@ -20,7 +20,8 @@ export default function Input(props) {
       name={name}
       value={value}
       onChange={onChange}
-      style={{ width: width && '100%' }}
+      style={{ width: width && '100%' , padding: padding && '0.5rem'}}
+      InputProps={InputProps}
     />
   );
 }
