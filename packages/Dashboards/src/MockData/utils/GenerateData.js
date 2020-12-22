f19c743d20c07fd4fda49128317bbfb837766c22
Raymond Huang <chialh@uci.edu>
2020-12-22 12:31:26 -0800

    Add similar Mock data

--- packages/Dashboards/src/MockData/utils/GenerateData.js
+++ packages/Dashboards/src/MockData/utils/GenerateData.js
@@ -18,6 +18,179 @@ const GenerateData = async () => {
     for (let i = 0; i < 5; i++) {
       // var BorrowerId = uuidv4();
 
+      var PrimaryBorrowerName = faker.name.findName();
+      var CoBorrowerName1 = faker.name.findName();
+      var CoBorrowerName2 = "";
+      var CoBorrowerName3 = "";
+      var CoBorrowerName4 = "";
+      var CoBorrowerName5 = "";
+      var GuarantorName1 = faker.name.findName();;
+      var GuarantorName2 = "";
+      var GuarantorName3 = "";
+      var GuarantorName4 = "";
+      var GuarantorName5 = "";
+
+      var PrimaryBorrowerBID = uuidv4();
+      var CoBorrowerBID1 = uuidv4();
+      var CoBorrowerBID2 = "";
+      var CoBorrowerBID3 = "";
+      var CoBorrowerBID4 = "";
+      var CoBorrowerBID5 = "";
+      var GuarantorBID1 = uuidv4();
+      var GuarantorBID2 = "";
+      var GuarantorBID3 = "";
+      var GuarantorBID4 = "";
+      var GuarantorBID5 = "";
+
+      var GuarantorType1 = "";
+
+      var PrimaryBorrowerTINSSN1 = faker.finance.account();
+      var CoBorrowerTINSSN1 = faker.finance.account();
+      var CoBorrowerTINSSN2 = "";
+      var CoBorrowerTINSSN3 = "";
+      var CoBorrowerTINSSN4 = "";
+      var CoBorrowerTINSSN5 = "";
+
+      var LoanID = uuidv4();
+      var possibleLoanType = ["Auto", "Mortgage", "Student"];
+      var pLTindex = Math.floor(Math.random() * 3);
+      var LoanType = possibleLoanType[pLTindex];
+      var OtherLoanType = "";
+
+      var CommitmentAmount = faker.finance.amount();
+      var OutstandingAmount = faker.finance.amount();
+      var yesOrNo = ["Yes", "No"];
+      var yOnIndex = Math.floor(Math.random() * 2);
+      var IsSublimit = yesOrNo[yOnIndex];
+
+      var MaturityTimeFrame = faker.date.past();
+      var MatiurityDate = faker.date.future();
+      var MaturityValue = faker.finance.amount();
+
+      var possibleInterestRateType = ["Fixed", "Variable", "Discount"];
+      var pIRTindex = Math.floor(Math.random() * 3);
+      var InterestRateType1 = possibleInterestRateType[pIRTindex];
+      var InterestRateType2 = "";
+      var InterestRateType3 = "";
+      var InterestRateType4 = "";
+      var InterestRateType5 = "";
+      var possibleInterestRate = ["3%", "2.25%", "1.25%"];
+      var pIRIndex = Math.floor(Math.random() * 3);
+      var InterestRate1 = possibleInterestRate[pIRIndex];
+      var InterestRate2 = "";
+      var InterestRate3 = "";
+      var InterestRate4 = "";
+      var InterestRate5 = "";
+
+      var InterestRateCeiling = "";
+      var InterestRateFloor = "";
+
+      var FeeType1 = "";
+      var FeeType2 = "";
+      var FeeType3 = "";
+      var FeeType4 = "";
+      var FeeType5 = "";
+      var FeeAmount1 = "";
+      var FeeAmount2 = ""; 
+      var FeeAmount3 = "";
+      var FeeAmount4 = "";
+      var FeeAmount5 = "";
+
+      var Purpose = "Need loan to use";
+      var DetailedPurpose = "";
+      var Syndication = "";
+      var LienPriority = "";
+      var CollateralType = "";
+
+      var RiskRating = Math.floor(Math.random() * 10);
+      var OriginalFacilityID = "";
+      var OriginationDate = "";
+      var LineReportedOnFrY9C = "";
+
+      var CostCenter;
+      do {
+        CostCenter = Math.floor(Math.random() * 999);
+      } while (CostCenter < 100);
+
+      var Division = "";
+      var LineOfBusiness = "";
+      var CumalitiveChargeOffs = "";
+      var ASC31010 = "";
+      var ASC31030 = "";
+
+      var NumberofDaysPrincialorInterestPastDue = "";
+      var NonAccrualDays = "";
+      var IDOfPrimaryObligorForRepayment = "";
+      var DateOfFinancials = "";
+      var DateOfLastAudit = "";
+      var NetSalesCurrent = "";
+      var NetSalesPriorYear = "";
+      var OperatingIncome = "";
+      var DepreciationAndAmortization = "";
+      var InterestExpense = "";
+      var NetIncomeCurrent = "";
+      var NetIncomePrioryYear = "";
+      var CashAndMarketableSecurities = "";
+      var AccountsReceivableCurrent = "";
+      var AccountsReceivablePriorYear = "";
+      var InventoryCurrent = "";
+      var InventoryPriorYear = "";
+      var CurrentAssetsCurrent = "";
+      var CurrentAssetsPriorYear = "";
+      var TangibleAssets = "";
+      var FixedAssets = "";
+      var TotalAssetsCurrent = "";
+      var TotalAssetsPriorYear = "";
+      var AccountsPayableCurrent = "";
+      var AccountsPayablePriorYear = "";
+      var ShortTermDebt = "";
+      var CurrentMaturitiesOfLongTermDebt = "";
+      var CurrentLiabilitiesCurrent = "";
+      var CurrentLiabilitiesPriorYear = "";
+      var LongTermDebt = "";
+      var MinorityInterest = "";
+      var TotalLiabilities = "";
+      var RetainedEarnings = "";
+      var CapitalExpenditures = "";
+      var SpecialPurposeEntityFlag = "";
+      var FairValueAdjustmentCommittedExposure = "";
+      var FairValueAdjustmentDrawn = "";
+      var LowerOfCostOrMarketFlag = "";
+      var SNCInternalCreditID = "";
+      var ProbabilityOfDefault = "";
+      var LossGivenDefault = "";
+      var ExposureAtDefault = "";
+      var RenewalDate = "";
+      var CreditFacilityCurrency = "";
+      var EstimatedValue = "";
+      var PrepaymentPenalty = "";
+      var PSRIndustryCode = "";
+      var ShareOfTotalCommitment = "";
+      var LeveragedLoan = "";
+      var DispositionFlag = "";
+      var DispostionScheduleShift = "";
+      var SyndicationType = "";
+      var TargetHold = "";
+      var FedCodes = "";
+      var CCARLiens = "";
+      var PropertyType = "";
+      var LoanPuproseRELOAN = "";
+      var LowerOfCostOrMarketFlag__1 = "";
+      var ISO4127CurrencyCodes = "";
+      var CurrentValueBasis = "";
+      var PrepaymentPenaltyFlag = "";
+      var LeveragedLoanFlag = "";
+      var TroubledDebtRestructuring = "";
+      var AcquiredLoan = "";
+      var Recourse = "";
+      var ParicipationFlag = "";
+      var IndustryCodeType = "";
+      var CreditFacilityType = "";
+      var CreditFacilityPurpose = "";
+      var LineReportedOnFRY9C = "";
+      var InterestIncomeTaxStatus = "";
+      var SyndicatedLoanFlag = "";
+
       // var RelationshipName = faker.company.companyName();
       // var BorrowerName = RelationshipName;
       // var DepositorName = RelationshipName;
@@ -80,9 +253,9 @@ const GenerateData = async () => {
         PrimaryBorrowerName,
         CoBorrowerName1,
         CoBorrowerName2,
-        CoBorrowerName2,
         CoBorrowerName3,
-        CoBorrowerName3,
+        CoBorrowerName4,
+        CoBorrowerName5,
         GuarantorName1,
         GuarantorName2,
         GuarantorName3,
@@ -222,7 +395,7 @@ const GenerateData = async () => {
         CreditFacilityType,
         CreditFacilityPurpose,
         LineReportedOnFRY9C,
-        interestIncomeTaxStatus,
+        InterestIncomeTaxStatus,
         SyndicatedLoanFlag,
 
         // ExternalSource: "FIS",
@@ -265,7 +438,7 @@ const GenerateData = async () => {
       };
       FISEntities.push(singleEntity);
     }
-
+    console.log(FISEntities)
     // var SalesForceEntities = ErrorFieldGenerator(FISEntities, "SalesForce");
     // var DataWarehouseEntities = ErrorFieldGenerator(FISEntities, "DataWarehouse");
     // var TemenosEntities = ErrorFieldGenerator(FISEntities, "Temenos");
@@ -292,4 +465,6 @@ const GenerateData = async () => {
   }
 };
 
-module.exports = GenerateData;
+GenerateData();
+
+// module.exports = GenerateData;
